7b692476e3cf52ba52d347290680e60dde17f5a3
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-23 20:52:46 -0300

    Update scripts.js

--- 5_string_avancado/scripts.js
+++ 5_string_avancado/scripts.js
@@ -1,5 +1,5 @@
 console.log('"primeira linha \n Segunda linha"');
 console.log("O meu nome é 'José'");
 console.log(`A multiplicação de 5  por 3  é ${5*3}`);
-console.log("O "+ "meu " + "nome "+ "é "+"jose.");
+console.log("O "+ "meu " + "nome "+ "é "+"jose Augusto.");
 
