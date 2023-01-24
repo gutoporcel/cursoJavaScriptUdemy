d1f2d266f952e40f40419f9a58cf470b8a6ae2d3
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-23 22:24:16 -0300

    Aula-8

---
+++ 8_boolean _op_logico/scripts.js
@@ -0,0 +1,8 @@
+console.log(5>3 && 3==2);
+console.log(5>3 || 3 ==1);
+
+console.log( 3==3 && "jose"=="jose");
+console.log(!(!(true && true)));
+
+console.log(true && true);
+console.log( false || false);
