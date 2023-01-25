b3b1b2e5a19623634f093f9da54e58d984541fcb
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-25 15:31:30 -0300

    2_aula-7

---
+++ Modulo_2/7_Math/script.js
@@ -0,0 +1,12 @@
+let maior =Math.max(1,2,3,6,10,500,7);
+console.log(maior);
+alert("O numero maior é: " + maior) 
+let menor = Math.min(0,2,4,3,-9,20);
+alert("o numero menor é: "+  menor);
+console.log(menor);
+let arrendondar = Math.round(6.9999991);
+console.log(arrendondar);
+alert(`o arrendondamen de 6,9999991 é: ${arrendondar}`);
+let arrendondarParaCima = Math.ceil(5.22224);
+console.log(arrendondarParaCima);
+alert("arrendamento para cima: "+ arrendondarParaCima);
