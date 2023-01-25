f4882fed7778e248b7d8d9c1b0d0094e19752bd9
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-25 16:19:31 -0300

    2_aula-10

---
+++ Modulo_2/10_else/script.js
@@ -0,0 +1,13 @@
+// Etrutura condiciona else
+let nome =prompt("Qual é seu nome?");
+if (nome =="jose") {
+    alert("Seu nome é jose ");
+    console.log(nome);
+    
+} else {
+alert("Seu nome nao é jose:");
+alert(`Seu nome é ${nome}`);
+console.log(nome);
+    
+}
+
