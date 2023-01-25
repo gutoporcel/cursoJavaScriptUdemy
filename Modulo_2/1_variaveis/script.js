89ba6f68ac2269d6d3cff53281b4867208e7c499
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-25 12:34:21 -0300

    2_aula-1

---
+++ Modulo_2/1_variaveis/script.js
@@ -0,0 +1,14 @@
+let nome = "Matheus";
+let laranjas = 5;
+let um=1, dois=2, tres=3;
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+console.log(laranjas*laranjas);
+nome ="joao";
+console.log(nome);
+laranjas = 10000;
+console.log(laranjas);
+laranjas = "laranja";
+console.log (laranjas);
+console.log(um+dois+tres);
