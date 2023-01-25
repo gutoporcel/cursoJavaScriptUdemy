3e3d514a03828d6df26005388701b0084d69af14
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-25 13:43:33 -0300

    2_aula-3

---
+++ Modulo_2/3_convencao/script.js
@@ -0,0 +1,10 @@
+let nome3 = 'teste';
+let $nome ="teste2";
+let _nome = "teste3";
+let  Nome= "teste4";
+let nomeCompletoComel = "camelCase";// mais indicado
+// variaveis  nao aceitas 
+
+// let 5nome = "teste";
+// let @nome = "teste";
+// let !nome = "teste";
