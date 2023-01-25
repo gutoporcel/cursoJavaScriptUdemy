6d777188052660343f8f73952903b0be5c9dcd07
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-25 19:38:38 -0300

    2_aula-19

---
+++ Modulo_2/19_switch/script.js
@@ -0,0 +1,26 @@
+// switch
+
+let nome ="matheus"
+
+switch (nome) {
+   case "matheus" :
+      console.log("O nome é Matheus");
+      break;
+   case "jose":
+      console.log("O nome é jose ");
+      break;
+   default:
+      console.log("O nome nao foi encontrado");
+      break;
+}
+
+// no caso do  if
+
+if (nome =="matheus") {
+   console.log("O nome é Matheus");
+}else if(nome== "jose"){
+   console.log("O nome é jose ");
+}
+else {
+   console.log("O nome nao foi encontrado"); 
+}
