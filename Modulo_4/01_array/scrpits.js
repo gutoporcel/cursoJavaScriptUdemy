76583d088f3fa5ef159dcf7548d2623b8dff4c49
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-29 21:32:41 -0300

    Modulo 4  liçao 1 ao 3

---
+++ Modulo_4/01_array/scrpits.js
@@ -0,0 +1,9 @@
+let arr = [1,2,5,6,7];
+let nomed = ["guto", "adriana", "monica", "sergio"];
+let bool = [true,false,false,true];
+let misturado =["guto", true,5];
+
+console.log(arr);
+console.log(nomed);
+console.log(bool);
+console.log(misturado);
---
+++ Modulo_4/02_array-2/scrpits.js
@@ -0,0 +1,17 @@
+let arr = [1,2,3,4,5,6,7];
+let nomed = ["guto", "adriana", "monica", "sergio"];
+let bool = [true,false,false,true];
+let misturado =["guto", true,5];
+
+
+console.log(arr[3]);
+console.log(arr.push(8));
+
+
+
+console.log(arr[1]);
+console.log(nomed[0]);
+console.log(bool[3]);
+console.log(arr.length );
+
+console.log(arr);
