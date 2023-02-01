0622e91c8eda4703a420b1e99a99bf823cc5f54c
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-01-31 22:11:05 -0300

    auala 5-6

---
+++ Modulo_4/05_objetos/scrpits.js
@@ -0,0 +1,10 @@
+let cachorro ={
+patas:4,
+nome:"rex",
+latir: function(){console.log("Au Au");}
+}
+console.log(cachorro.patas);
+console.log(cachorro);
+console.log(cachorro.nome);
+cachorro.latir();
+
---
+++ Modulo_4/06_objetos_criar_prop/scrpits.js
@@ -0,0 +1,16 @@
+let pessoa = {
+nome:"jose augusto",
+idade: 30,
+profissão : "montador"
+
+
+}
+
+console.log(pessoa.nome);
+
+delete pessoa.nome;
+
+console.log(pessoa.nome);
+pessoa.casado=false;
+
+console.log(pessoa.casado)
