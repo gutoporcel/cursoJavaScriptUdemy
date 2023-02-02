b7aade7c1fd2e1ee796b049dc3a25c6870bee7f3
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-02 20:08:38 -0300

    aula-10

---
+++ Modulo_4/10_loop_arrays/scrpits.js
@@ -0,0 +1,20 @@
+ let nome =["maria","jose","joao"];
+
+for (let i = 0; i <= nome.length; i++) {
+    console.log(nome[i]);
+    
+}
+
+let animal = [
+    "cao",
+    "rex",
+    25,
+
+];
+
+
+for (let i = 0; i <= animal.length; i++) {
+    console.log(animal[i]);
+    
+}
+
