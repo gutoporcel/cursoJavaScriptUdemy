9b5912a4f3a719efec77857f8f8560df92fd6591
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-04 15:14:35 -0300

    Aula-24

---
+++ Modulo_4/24_destructuing_obj/scrpits.js
@@ -0,0 +1,11 @@
+let obj ={
+    rodas:4,
+    portas:4,
+    tetosolar:true,
+    motor: '2.0'
+}
+
+const {rodas: vrodas, portas: vportas, tetosolar: vtetosolar, motor:vmotor }=  obj;
+
+console.log(vrodas);
+console.log(vmotor);
