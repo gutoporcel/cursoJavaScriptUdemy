4c6abb940d80bed2dae9415631d305729a83e976
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-04 14:40:23 -0300

    aula-20

---
+++ Modulo_4/20_split/scrpits.js
@@ -0,0 +1,14 @@
+let frase =  "O rato roeu a roupa do rei de roma";
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let frase1 =" Testando o metodo split";
+
+console.log(frase1.split(" "));
+
+
+let objt = "bola,carro,arroz,feijao";
+console.log(objt.split(","));
+
