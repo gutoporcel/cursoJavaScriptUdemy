1ca3ddcab61cb5a4935d55615f1b5f7fe2af2f8d
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-04 15:03:45 -0300

    Aula-23

---
+++ Modulo_4/23_rest_operator/scrpits.js
@@ -0,0 +1,20 @@
+let n1 = 1;
+let n2 = 4;
+let n3 = 8;
+let n4 = 6;
+let n5 = 2;
+let n6 = 7;
+let n7 = 14;
+
+
+function imprimeNumeros(...args){
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+        
+    }
+
+
+
+}
+
+imprimeNumeros(n1,n2,n3,n4,n5,n6,n7);
