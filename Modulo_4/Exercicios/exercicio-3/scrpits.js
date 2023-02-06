f2240a4f48b0e29aec1e2dd0b57e053f7e05b0c2
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-06 20:58:50 -0300

    exercicio-04

--- Modulo_4/Exercicios/exercicio-3/scrpits.js
+++ Modulo_4/Exercicios/exercicio-3/scrpits.js
@@ -2,7 +2,7 @@ let onibus = {
 'rodas':8,
 'passageiros':40,
 'portas':2
-}
+};
 
 console.log(onibus);
 console.log(onibus.passageiros);
---
+++ Modulo_4/Exercicios/exercicio-4/scrpits.js
@@ -0,0 +1,12 @@
+let onibus = {
+'rodas':8,
+'passageiros':40,
+'portas':2
+}
+onibus.janelas=20;
+//console.log(onibus.includes("janelas"));
+console.log(onibus.janelas);
+console.log(onibus.portas);
+delete onibus.rodas;
+console.log(onibus.rodas);
+console.log(onibus);
