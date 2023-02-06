e7f3056e29f820666f93ecaeba944656de1489b7
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-06 20:28:09 -0300

    exercicio-02

--- Modulo_4/Exercicios/exercicio-2/scrpits.js
+++ Modulo_4/Exercicios/exercicio-2/scrpits.js
@@ -1,20 +1,5 @@
-let pessoa  = {
-"nome":"jose",
-"sobrenome":"augusto",
-"idade":25,
-"profissao":"montador",
-"estudante":true,
-"hobbies":["Video game","leitura","correr"]
-}
+let arr =['fox','agile','fiesta','ka'];
+let arr2 =[1,2]
 
-let pessoTexto = JSON.stringify(pessoa);
-//vira uma string
-console.log(pessoTexto);
-
-let pessoaJSON = JSON.parse(pessoTexto);
-
-console.log(pessoaJSON);
-console.log(pessoaJSON.hobbies[0]);
-
-
-
+console.log(arr.length);
+console.log(arr2.length);
