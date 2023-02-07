1332c4b73b305073a96e767771b44b2bf7f8c50b
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-07 20:08:25 -0300

    Exercicio-8

---
+++ Modulo_4/Exercicios/exercicio-8/scrpits.js
@@ -0,0 +1,17 @@
+let receita ={
+
+"arroz":"1 concha",
+"feijao":"1 concha",
+"salada":"1 prato",
+"homelete":"1 pedaço",
+"suco":"1 copo",
+"pessoas":2
+
+
+};
+
+console.log(receita.arroz);
+console.log(receita.feijao);
+console.log(receita.homelete);
+console.log(receita.salada);
+console.log(receita.pessoas);
