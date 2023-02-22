231c47c235cc97f17d89e912bcb4a9d89ce92614
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-22 20:13:22 -0300

    06_operador_plus

---
+++ Modulo_7/06_operador_plus/scrpts.js
@@ -0,0 +1,6 @@
+const umOuMaisNumeros =/\d+/;
+
+console.log(umOuMaisNumeros.test("123"));
+console.log(umOuMaisNumeros.test(""));
+console.log(umOuMaisNumeros.test("dsaddasdasd"));
+console.log(umOuMaisNumeros.test("dsaddasdasddsdsds1324845"));
