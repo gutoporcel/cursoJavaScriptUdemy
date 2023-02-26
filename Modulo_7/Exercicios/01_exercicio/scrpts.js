797cd143a42a844184efa9487d798860bd456a7e
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-26 19:53:11 -0300

    Exercicio_01

---
+++ Modulo_7/Exercicios/01_exercicio/scrpts.js
@@ -0,0 +1,12 @@
+const soMaiscula = /[^a-z][A-Z]/;
+
+console.log(soMaiscula.test("asdssada"));
+console.log(soMaiscula.test("ASDASFSADFSDFDSA"));
+console.log(soMaiscula.test("AS123456"));
+console.log(soMaiscula.test("Asdsdsd"));
+console.log(soMaiscula.test("sdasdasdA"));
+console.log(soMaiscula.test("AAAAA"));
+///////////////////////////////////////////
+
+
+
