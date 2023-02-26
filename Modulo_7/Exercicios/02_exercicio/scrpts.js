85ad1a07a228257a9a6a727757a58293d676980c
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-26 20:17:41 -0300

    Exercicio_02

---
+++ Modulo_7/Exercicios/02_exercicio/scrpts.js
@@ -0,0 +1,13 @@
+const ID = /\d+ID\b/;
+
+console.log(ID.test("asdssadaid"));
+console.log(ID.test("ASDASFSADFSDFDSAID"));
+console.log(ID.test("AS123456"));
+console.log(ID.test("AsdsdsIddddd"));
+console.log(ID.test("sidiasdasddddIDD"));
+console.log(ID.test("IDidAAAAA"));
+console.log(ID.test("00000000ID"));
+///////////////////////////////////////////
+
+
+
