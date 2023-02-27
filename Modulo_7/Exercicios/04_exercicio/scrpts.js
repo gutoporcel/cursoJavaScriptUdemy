206e2b244e6065da540efe1a28178db1fa8a5338
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-02-27 20:33:06 -0300

    Exercicio_4

--- Modulo_7/Exercicios/04_exercicio/scrpts.js
+++ Modulo_7/Exercicios/04_exercicio/scrpts.js
@@ -1,9 +1,8 @@
-const reg = /Marca: (Nike|Adidas|Puma|Asics)/;// marca:
+//const valIP = /[0-9]{3}[.][0-9]{1}[.][0-9]{1}[.][0-9]{1}/;
+const valIP = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
 
-console.log(reg.test("Marca: Nike")); //true
-console.log(reg.test("Marca: Adidas"));//true
-console.log(reg.test("Marca: Puma"));//true
-console.log(reg.test("Marca: Asics"));//true
-console.log(reg.test("Marca: nikeee"));//false
-console.log(reg.test("Marca: nike"));//false
-console.log(reg.test("Marca: nike"));//true
+console.log(valIP.test("111.1.0.0"));
+console.log(valIP.test("152.10.323.32"));
+console.log(valIP.test("152.1.1.1"));
+console.log(valIP.test("123.123.123.123"));
+console.log(valIP.test("8.8.8.8"));
