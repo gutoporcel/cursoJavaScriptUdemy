dc3bacaf06532eb6c1724cf3ce66b900dd0a9d27
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-02 20:37:57 -0300

    01_callback

---
+++ Modulo_8/01_callback/scrpts.js
@@ -0,0 +1,14 @@
+console.log("1");
+
+setTimeout(function(){
+console.log("Ola MUndo");
+},2000);
+
+setTimeout(function(){
+    alert("Ola MUndo");
+    },5000);
+
+console.log("2");
+console.log("3");
+console.log("4");
+
