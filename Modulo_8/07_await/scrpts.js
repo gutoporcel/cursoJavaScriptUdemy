15dae81a87e311f604e4a243af77170fb580f19c
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-03 19:58:38 -0300

    07_await

---
+++ Modulo_8/07_await/scrpts.js
@@ -0,0 +1,18 @@
+function somComDelay(a,b) {  
+    return new Promise (resolve =>{
+        setTimeout(function(){
+            resolve(a+b);
+
+        },4000);
+    });  
+}
+
+async function resSoma(a,b,c) {
+
+    let x = somComDelay(a,b);
+    let y = c;
+
+    return await x + y
+}
+
+resSoma(1,2,3).then(value =>console.log(value));
