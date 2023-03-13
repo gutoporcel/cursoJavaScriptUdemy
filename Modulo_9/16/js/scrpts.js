554f73af3ff70ac8dcd3ff8ec11cc95a2b891f8a
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-13 19:09:45 -0300

    16_mudando_estilos

--- Modulo_9/16/js/scrpts.js
+++
@@ -1,7 +0,0 @@
-elemento = document.querySelector("#titulo-principal");
-
-console.log("Largura: " + elemento.offsetWidth);// considera as bordas
-console.log("Altura: " +elemento.offsetHeight);
-
-console.log("Largura: " + elemento.clientWidth);//nao considera as bordas
-console.log("Altura: " +elemento.clientHeight);
