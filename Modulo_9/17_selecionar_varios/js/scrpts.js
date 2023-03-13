cc215edaff6da0f30c5eb732b89e321b3d25ffef
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-13 19:17:01 -0300

    17_seliconar_varios

---
+++ Modulo_9/17_selecionar_varios/js/scrpts.js
@@ -0,0 +1,6 @@
+let itens = document.querySelectorAll(".itens-vermelhos");
+
+console.log(itens);
+itens[0].style.color = "red";
+let itens2 = document.querySelectorAll(".itens-azuis");
+console.log(itens2);
