cf2003a9df1ac006e9dfaa38b686aae8fb228ecf
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-13 18:48:23 -0300

    15_posiçoes

--- Modulo_9/15_posiçoes/js/scrpts.js
+++ Modulo_9/15_posiçoes/js/scrpts.js
@@ -1,7 +1,3 @@
 let elemento = document.querySelector("#titulo-principal");
 
-console.log("Largura: " + elemento.offsetWidth);// considera as bordas
-console.log("Altura: " +elemento.offsetHeight);
-
-console.log("Largura: " + elemento.clientWidth);//nao considera as bordas
-console.log("Altura: " +elemento.clientHeight);  
+console.log(elemento.getBoundingClientRect());
