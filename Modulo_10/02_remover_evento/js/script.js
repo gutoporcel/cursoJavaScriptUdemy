efb44e95ce3a3a51b6b551b71471bed03a7ef241
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-16 20:08:26 -0300

    02_removendo_evento

---
+++ Modulo_10/02_remover_evento/js/script.js
@@ -0,0 +1,13 @@
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+
+
+function msg(){
+    console.log("Achou")
+}
+btn1.addEventListener("click", msg);
+
+btn2.addEventListener("click",()=>{
+
+    btn1.removeEventListener("click",msg);
+});
