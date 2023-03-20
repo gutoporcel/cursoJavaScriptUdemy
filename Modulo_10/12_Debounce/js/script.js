280134953bcab9f8202e0d610f4cb3e935e46184
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-20 20:57:06 -0300

    12_debounce

---
+++ Modulo_10/12_Debounce/js/script.js
@@ -0,0 +1,10 @@
+let timeout;
+window.addEventListener("mousemove",function (e){
+    this.clearTimeout(timeout);
+    timeout = setTimeout(function(){
+        console.log(e.x);
+    },500)
+   
+});
+
+
