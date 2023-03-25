cf691a234fd588b69ab0e46f4f3e39220fe5fe76
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-25 11:35:12 -0300

    Implementaçao da logica do jogo variaveis, metodos de quem esta jogando primeiro e alterna entre jogadores colcando na tela x e o

--- projeto_01/js/scrpits.js
+++ projeto_01/js/scrpits.js
@@ -1,4 +1,62 @@
-alert("hello World!!");
-alert("hello World!!");
-alert("hello World!!");
-alert("hello World!!");             
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messagContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+//contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+//adicionando o evento de click
+
+for (let i = 0; i< boxes.length;i++){
+
+   //quando alguem clica na caixa 
+    boxes[i].addEventListener("click", function(){
+        let el = checkEL(player1,player2);
+
+        //verefica se ja tem x ou o
+        if(this.childNodes.length == 0){
+            let cloneEl = el.cloneNode(true);
+            this.appendChild(cloneEl);
+    
+            //computar jogada
+    
+            if(player1 == player2){
+                player1++;
+    
+            }else{
+    
+                player2++;
+            }
+    
+    
+    
+
+
+        }
+
+      
+
+    });
+
+
+}
+
+//  Ver quem vai jogar
+
+function checkEL(player1,player2){
+
+    
+    if(player1 == player2) {
+        //x
+        el=x;
+    }else{
+        //o
+        el =o;
+    }
+    return el;
+}
