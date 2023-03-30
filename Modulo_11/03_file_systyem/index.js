4d3c2a224e7d26a04c42261be2fa368361d18a0f
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-03-30 20:11:30 -0300

    escrevendo no arquivo

--- Modulo_11/03_file_systyem/index.js
+++ Modulo_11/03_file_systyem/index.js
@@ -1,11 +1,21 @@
 
-let {readFile} =require('fs');
+let {readFile, writeFile  } = require('fs');
 
 readFile("arquivo.txt","utf8", (error, texto)=>{
     if(error){
         throw error;
     }else{
         console.log(texto);
+    }
+});
+
+
+writeFile("arquivo.txt","escrevendo no aquivo Hello Wolrd",(error)=>{
+
+    if (error) {
+        throw error;
+    } else {
+        console.log("escreveu com sucesso");
     }
 
 
