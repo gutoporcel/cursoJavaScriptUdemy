dd5101fc36005bab9ced5c27257abf110a982691
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-04-03 20:56:58 -0300

    Projeto 2 express e banco sqlite contados

---
+++ projeto_02/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const db = require ('./db/connection');
+
+
+app.listen(PORT, function(){
+    console.log(`O Express esta rodando na porta ${PORT}`);
+});
+
+//db connection
+db
+.authenticate()
+.then(()=>{
+    console.log("conectou  ao banco com sucesso");
+
+})
+.catch(err =>{
+
+    console.log("ocorreu um erro ao conectar");
+
+});
+
+// routes
+app.get('/',(req,res)=>{
+    res.send("Esta funcionado 3");
+});
+
