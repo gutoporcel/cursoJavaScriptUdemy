f047df0d5d3d77b3ef24a06ac0a73d8e1c5fd1c2
jose Augusto Porcel stacio <porcel_augusto@hotmail.com>
2023-04-25 18:55:53 -0300

    Projeto 02 resgatando dados do banco de dados

--- projeto_02/app.js
+++ projeto_02/app.js
@@ -1,23 +1,24 @@
-const express    = require('express');
-const exphbs     = require('express-handlebars');
-const app        = express();
-const path       = require('path')
-const PORT       = 3000;
-const db         = require ('./db/connection');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const path = require('path')
+const PORT = 3000;
+const db = require('./db/connection');
 const bodyParser = require('body-parser');
+const Job = require('./models/Job');
 
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log(`O Express esta rodando na porta ${PORT}`);
 });
 
 //body parser
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //handle bars
-app.set('views',path.join(__dirname,'views'));
-app.engine('handlebars', exphbs.engine({defaultLayout:'main'}));
-app.set('view engine','handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,25 +27,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //db connection
 db
-.authenticate()
-.then(()=>{
-    console.log("conectou  ao banco com sucesso");
+    .authenticate()
+    .then(() => {
+        console.log("conectou  ao banco com sucesso");
 
-})
-.catch(err =>{
+    })
+    .catch(err => {
 
-    console.log("ocorreu um erro ao conectar");
+        console.log("ocorreu um erro ao conectar");
 
-});
+    });
 
 // routes
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get('/', (req, res) => {
+
+    Job.findAll({
+        order: [
+            ['createdAt', 'DESC']
+        ]
+    })
+        .then(jobs => {
+
+            res.render('index', {
+                jobs
+            });
+
+        });
+
 });
 
 
 //jobs routes
 
-app.use('/jobs',require('./routes/jobs'));
+app.use('/jobs', require('./routes/jobs'));
 
 //fim   
